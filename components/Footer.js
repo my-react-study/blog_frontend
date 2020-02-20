7f2cb88a9f44d402225e4361ddcdc7668bbb60f4
tangmingheng <mhtang@thoughtworks.com>
2020-02-20 22:55:01 +0800

    Footer组件制作

---
+++ components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../public/style/components/Footer.css"
+const Footer = () => (
+    <div className="footer-div">
+        <div>系统由 React+Node+Ant Desgin驱动 </div>
+        <div>JSPang.com</div>
+    </div>
+)
+
+export default Footer
